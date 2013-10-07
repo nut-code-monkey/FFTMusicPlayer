86e9dafd70eeef7a754d857126f8fe98611f4126
Max Lunin <nut.code.monkey@gmail.com>
2013-10-07 15:01:29 +0300

    Initial Commit

---
+++ FFTMusicPlayer/FFTMusicPlayer.h
@@ -0,0 +1,13 @@
+//
+//  FFTMusicPlayer.h
+//  FFTMusicPlayer
+//
+//  Created by Max on 07.10.13.
+//  Copyright (c) 2013 Max Lunin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface FFTMusicPlayer : NSObject
+
+@end
---
+++ FFTMusicPlayer/FFTMusicPlayer.m
@@ -0,0 +1,13 @@
+//
+//  FFTMusicPlayer.m
+//  FFTMusicPlayer
+//
+//  Created by Max on 07.10.13.
+//  Copyright (c) 2013 Max Lunin. All rights reserved.
+//
+
+#import "FFTMusicPlayer.h"
+
+@implementation FFTMusicPlayer
+
+@end
